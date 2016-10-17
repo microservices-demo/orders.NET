d74878cfdad90f0bf903cc1f0ac6244642092e6e
Lukasz Guminski <lguminski@gmail.com>
2016-10-17 20:34:23 +0200

    Adding build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,9 @@
 
 set -ev
 
+git submodule update --init --recursive
+rm -fr HalKit/tests/
+
 SCRIPT_DIR=$(dirname "$0")
 
 if [[ -z "$GROUP" ]] ; then
@@ -21,9 +24,6 @@ else
 fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
-$DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -DskipTests package
-
-cp $CODE_DIR/target/*.jar $CODE_DIR/docker/$(basename $CODE_DIR)
 
 for m in ./docker/*/; do
     REPO=${GROUP}/$(basename $m)
